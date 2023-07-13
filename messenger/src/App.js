af2b47f2bce55c5f3a2ade90e78f5b067ff94932
rabia1311 <rabiasultana.n@gmail.com>
2023-07-13 23:11:58 +1030

    started creating sidebar

--- messenger/src/App.js
+++ messenger/src/App.js
@@ -1,5 +1,13 @@
 import "./App.css";
-
+import React from "react";
+import { Sidebar, Menu, MenuItem, useProSidebar } from "react-pro-sidebar";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
+import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
+import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
+import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
+import HelpOutlineOutlinedIcon from "@mui/icons-material/HelpOutlineOutlined";
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
 function App() {
   return <h1> Chat Messenger </h1>;
 }
