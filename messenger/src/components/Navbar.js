313640c5991f3b525dcaca66795224700784bbef
rabia1311 <rabiasultana.n@gmail.com>
2023-07-17 22:04:25 +1030

    search and chat dfiexed

--- messenger/src/components/Navbar.js
+++ messenger/src/components/Navbar.js
@@ -1,13 +1,20 @@
 import React from "react";
 import GroupAddOutlinedIcon from "@mui/icons-material/GroupAddOutlined";
 import AddCircleOutlineOutlinedIcon from "@mui/icons-material/AddCircleOutlineOutlined";
+
 const Navbar = () => {
   return (
-    <div className="navbar">
+    <div
+      className="navbar"
+      style={{ display: "flex", justifyContent: "space-between" }}
+    >
       <span className="pogo">
         <h2>Chat</h2>
       </span>
-      <div className="user">
+      <div
+        className="user"
+        style={{ marginTop: "24px", justifyContent: "space-between" }}
+      >
         <span>
           <GroupAddOutlinedIcon />
         </span>
--- messenger/src/components/Searchbar.js
+++ messenger/src/components/Searchbar.js
@@ -5,9 +5,32 @@ const Searchbar = () => {
     <div className="sidebar">
       <div className="navbar">
         <div className="user">
-          <div className="search">
-            <div className="searchform">
-              <input type="text" placeholder="Search Chats" />
+          <div
+            className="search"
+            style={{ display: "flex", justifyContent: "center" }}
+          >
+            <div
+              className="searchform"
+              style={{
+                width: "300px",
+                height: "60px",
+                display: "flex",
+                alignItems: "center",
+              }}
+            >
+              <input
+                type="text"
+                placeholder="Search Chats"
+                style={{
+                  flex: 1,
+                  border: "none",
+                  width: "300px",
+                  color: "whitesmoke",
+                  height: "40px",
+                  outline: "none",
+                  paddingLeft: "6px",
+                }}
+              />
             </div>
           </div>
         </div>
