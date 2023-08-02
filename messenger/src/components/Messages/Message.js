cb22b46376f5b34990d5e0cf8f4aecb80203fc70
rabia1311 <rabiasultana.n@gmail.com>
2023-08-02 15:49:34 +0530

    the messages with chat_id shown in console

--- messenger/src/components/Messages/Message.js
+++ messenger/src/components/Messages/Message.js
@@ -2,7 +2,28 @@ import React from "react";
 import Chatmsg from "../Chatmsg";
 import "../Messages/msg.css";
 import Reply from "../Reply";
+import { useEffect } from "react";
 const Message = ({ receivedMessage, chatConversation, receiver }) => {
+  // Function to fetch the API and print the content in the console
+  const fetchMessages = async () => {
+    try {
+      const response = await fetch("http://localhost:4000/chat/messages");
+      if (response.ok) {
+        const data = await response.json();
+        console.log("API Response:", data);
+      } else {
+        console.log("Failed to fetch messages.");
+      }
+    } catch (error) {
+      console.error("Error fetching messages:", error);
+    }
+  };
+
+  // Call the fetchMessages function once the component is mounted
+  useEffect(() => {
+    fetchMessages();
+  }, []);
+
   console.log(receivedMessage);
   console.log(chatConversation);
   console.log(receiver);
